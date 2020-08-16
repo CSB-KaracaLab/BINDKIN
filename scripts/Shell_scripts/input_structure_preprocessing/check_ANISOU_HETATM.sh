9232ed4537bc71b6293ad2e4774a718c8a6d3d06
MehmetErguven <58887483+MehmetErguven@users.noreply.github.com>
2020-08-17 00:10:02 +0300

    script directory update
    The "script_etc" directory was reorganized.

--- scripts/Shell_scripts/input_structure_preprocessing/check_ANISOU_HETATM.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#For each pdb file in a given directory, this script checks the presence of the terms "ANISOU" and "HETATM".
-
-for i in *.pdb;
-do
-	grep ANISOU *.pdb
-	grep HETATM *.pdb
-done
